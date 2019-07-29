1645fc46a2f8cb721a37eb5c65104d5a3bc2fe69
Vishaali Kadambur Srikanth <vs255542@dal.ca>
2019-07-29 11:27:47 -0300

    Webhook call

--- insurance/api/controllers/InsuranceController.js
+++ insurance/api/controllers/InsuranceController.js
@@ -5,6 +5,7 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+var request = require('request');
 module.exports = {
     calculate: async function(req,res)
     {
@@ -18,20 +19,29 @@ module.exports = {
 
         await Insurance.createEach([{mort_id:mort_id, appraisal_value: appraisal_value, insured_value:insured_value, deductible_value:deductible_value, mlsid: mlsid, name: name}]);
 
-         return res.json(
-         {
-             name: name,
-             mort_id: mort_id,
-             mlsid: mlsid,
-             insured_value: insured_value,
-             deductible_value: deductible_value,
+        //  return res.json(
+        //  {
+        //      name: name,
+        //      mort_id: mort_id,
+        //      mlsid: mlsid,
+        //      insured_value: insured_value,
+        //      deductible_value: deductible_value,
              
-         });
+        //  });
 
-         
-      
-    },  
+        url="https://prod-04.canadacentral.logic.azure.com:443/workflows/4deb1889c4e94d59b3ca84e01cfca964/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=CczRwFDUyTlPk4ptdV_7WFCVHIvlHMzRuObumI-WbXE";
+
+        request.post(url, { json : { "mort_id" : mort_id ,"appraisal_value":appraisal_value, "insured_value": insured_value , "deductible_value": deductible_value, mlsid: mlsid,"name": name}}, function(error, response, body) {
   
-
-};
-
+            if (error) {
+              sails.log.error(error);
+              return res.json(500, { error: 'Unable to make post call' });
+            }
+            else {
+                return res.json({"status": 'success'});
+            }
+            });
+           
+        }
+    };      
+    
