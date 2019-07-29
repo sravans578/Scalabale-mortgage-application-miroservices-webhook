d3cba7f208ef83dfe680a33278a1bb6395355f5b
aparnasweth <ap500517@dal.ca>
2019-07-29 12:53:10 -0300

    mbr backend commit

---
+++ MBR_Backend/api/models/PersonalInfo.js
@@ -0,0 +1,48 @@
+/**
+ * PersonalInfo.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+    name:{
+        type : 'string',
+        required: true
+    },
+    mortgage_value:{
+        type:'string',
+        required: true
+    },
+    MISID:{
+        type:'string',
+        required: true
+    },
+    Employer_name:{
+        type:'string',
+        required: true
+    },
+    Password:{
+        type:'string',
+        required: true
+    },
+    // RE_status:{
+    //     type:'string',
+    //     defaultsTo: "Pending"
+    // },
+    employeer_status:{
+        type:'string',
+        defaultsTo: "Pending"
+    },
+    insurance_value:{
+        type:'string',
+        defaultsTo: "Pending"
+    },
+    deduct_value:{
+        type:'string',
+        defaultsTo: "Pending"
+    }
+}
+};
---
+++ MBR_Backend/config/http.js
@@ -0,0 +1,87 @@
+/**
+ * HTTP Server Settings
+ * (sails.config.http)
+ *
+ * Configuration for the underlying HTTP server in Sails.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For more information on configuration, check out:
+ * https://sailsjs.com/config/http
+ */
+
+
+module.exports.http = {
+
+  /****************************************************************************
+   *                                                                           *
+   * Sails/Express middleware to run for every HTTP request.                   *
+   * (Only applies to HTTP requests -- not virtual WebSocket requests.)        *
+   *                                                                           *
+   * https://sailsjs.com/documentation/concepts/middleware                     *
+   *                                                                           *
+   ****************************************************************************/
+
+  middleware: {
+
+    /***************************************************************************
+     *                                                                          *
+     * The order in which middleware should be run for HTTP requests.           *
+     * (This Sails app's routes are handled by the "router" middleware below.)  *
+     *                                                                          *
+     ***************************************************************************/
+
+    order: [
+      'cookieParser',
+      'session',
+      'bodyParser',
+      'compress',
+      'logger',
+      'poweredBy',
+      'router',
+      'www',
+      'favicon',
+    ],
+
+    logger: async function(req, res, next) {
+      console.log('Initializing `logger` (HTTP middleware)...');
+      console.log('Received HTTP request: ' + req.method + ' ' + req.path + ' ' + req.ip);
+      res.on('finish', async function() {
+        console.log('Sent HTTP response: ' + res.statusCode);
+
+        await Logger.create({
+          IP: req.ip,
+          requestUrl: req.path,
+          requestBody: JSON.stringify(req.body),
+          method: req.method,
+          requestHeaders: JSON.stringify(req.headers),
+          responseTime: new Date() - req._startTime + ' ms',
+          responseCode: res.statusCode,
+          appSource: 'Mortgage Portal'
+        }).exec(function(err, result) {
+          if (err) {
+            console.log('Some error occured ' + err);
+          }
+        });
+      })
+
+      return next();
+    },
+
+
+    /***************************************************************************
+     *                                                                          *
+     * The body parser that will handle incoming multipart HTTP requests.       *
+     *                                                                          *
+     * https://sailsjs.com/config/http#?customizing-the-body-parser             *
+     *                                                                          *
+     ***************************************************************************/
+
+    // bodyParser: (function _configureBodyParser(){
+    //   var skipper = require('skipper');
+    //   var middlewareFn = skipper({ strict: true });
+    //   return middlewareFn;
+    // })(),
+
+  },
+
+};
